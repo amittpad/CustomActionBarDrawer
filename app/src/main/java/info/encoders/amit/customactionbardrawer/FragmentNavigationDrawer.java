a0ecd943b9d6cc398f1bc6d20e22942058fb86ad
Amit <pramanikamit093@gmail.com>
2017-10-17 18:13:21 +0530

    Initial commit

---
+++ app/src/main/java/info/encoders/amit/customactionbardrawer/FragmentNavigationDrawer.java
@@ -0,0 +1,25 @@
+package info.encoders.amit.customactionbardrawer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Admin on 17-10-2017.
+ */
+
+public class FragmentNavigationDrawer extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+    }
+
+}
